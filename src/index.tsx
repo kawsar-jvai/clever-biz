5607e9cd6e1abfb9448504ec02ac30783d44a6f3
Kawsar <diu.kawsar@gmail.com>
2025-04-08 20:58:39 +0600

    tailwind added

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+const IndexPage = () => {
+  return (
+    <>
+      <header className=""></header>
+      <main>Index</main>
+    </>
+  );
+};
+
+export default IndexPage;
---
+++ src/store/main-slice.tsx
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const mainSlice = createSlice({
+  name: "main",
+  initialState: {},
+  reducers: {},
+});
+
+export {};
+
+export default mainSlice.reducer;
